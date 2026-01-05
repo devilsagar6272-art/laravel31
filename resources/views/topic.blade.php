e4925cd954a4f5327ab93a841429f281316feca9
sagar-ambaliya <devilsagar6272-art@users.noreply.github.com>
2026-01-05 14:09:42 +0530

    intil laravel

--- resources/views/topic.blade.php
+++ resources/views/topic.blade.php
@@ -14,4 +14,36 @@ case '2':
 default:
  echo "the end";
 }
+// forloop
+for ($i = 5; $i >= 1; $i--) {
+    for ($j = 1; $j <= $i; $j++) {
+        echo "*";
+    }
+    echo "<br>";
+}
+$a=1;
+while($a < 10)
+{
+  $b=1;
+  while($b<$a)
+  {
+    echo "x";
+    $b++;
+  }
+    $a++;
+ echo "<br>";
+}
+$rows = 10;
+for($i=1;$i<=$rows;$i++)
+{
+    for($space=$rows;$space> $i;$space--)
+    {
+        echo "&nbsp;";
+    }
+    for($star=1;$star <= (2*$i-1);$star++)
+    {
+        echo "v";
+    }
+    echo "<br>";
+}
 ?>
