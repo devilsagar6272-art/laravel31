33ccd75a8988ad73227dc24b38f540c5b69c56a2
sagar-ambaliya <devilsagar6272-art@users.noreply.github.com>
2026-01-05 16:44:22 +0530

    init laravel-project

--- resources/views/topic.blade.php
+++ resources/views/topic.blade.php
@@ -21,13 +21,13 @@ for ($i = 5; $i >= 1; $i--) {
     }
     echo "<br>";
 }
-$a=1;
-while($a < 10)
+$a=65;
+while($a < 69)
 {
-  $b=1;
+  $b=65;
   while($b<$a)
   {
-    echo "x";
+    echo chr($b);
     $b++;
   }
     $a++;
@@ -43,6 +43,39 @@ for($i=1;$i<=$rows;$i++)
     for($star=1;$star <= (2*$i-1);$star++)
     {
         echo "v";
+    }
+    echo "<br>";
+}
+
+for($a=65;$a < 69;$a++)
+{
+    for($b=65;$b < $a;$b++)
+    {
+        echo chr($b);
+    }
+    echo "<br>";
+}
+for($a=1;$a<=10;$a++)
+{
+    for($b=10;$b>$a;$b--)
+    {
+       echo "&nbsp;&nbsp;";
+    }
+    for($c=1;$c<=(2*$a-1);$c++)
+    {
+      echo $a;
+    }
+    echo "<br>";
+}
+for($a=10;$a>=1;$a--)
+{
+    for($b=10;$b>$a;$b--)
+    {
+       echo "&nbsp;&nbsp;";
+    }
+    for($c=1;$c<=(2*$a-1);$c++)
+    {
+      echo $a;
     }
     echo "<br>";
 }
