5d28b79bd6c6099a7b4f36b7432d137ce25f75a8
sagar-ambaliya <devilsagar6272-art@users.noreply.github.com>
2026-01-20 10:51:29 +0530

    esage

--- resources/views/Encapsulation.blade.php
+++ resources/views/Encapsulation.blade.php
@@ -1,5 +1,10 @@
+<form>
+<input type="text" id="sagar">
+<input type="text" id="sagararar">
+<input type="sumbit">
+</form>
 <?php
-class het
+/*class het
 {
 private $balance = 10000;
  function getvlaue()
@@ -16,4 +21,72 @@ private $balance = 10000;
 }
 $obj = new het();
 $obj->drawble(700); 
-echo $obj->getvlaue();
+echo $obj->getvlaue();*/
+/*class name 
+{
+   function show()
+   {
+      echo 1;
+   }
+}
+$obj = new sagar();
+$obj->show();
+$user = usermodel::create({
+ 'name'=>$request->name,
+ 'email'=>$request->email
+});*/
+for($a=1;$a < 10 ; $a++)
+   {
+      for($b=10; $b > $a; $b--)
+      {
+      echo "&nbsp;&nbsp";
+      }
+      for($c=1;$c<=(2*$a-1);$c++)
+         {
+            echo  "*";
+         }
+      echo "<br>";
+   }
+   for($a=10;$a > 1 ; $a--)
+   {
+      for($b=10; $b > $a; $b--)
+      {
+      echo "&nbsp;&nbsp";
+      }
+      for($c=1;$c<=(2*$a-1);$c++)
+         {
+            echo  "*";
+         }
+      echo "<br>";
+   }
+?>
+<script>
+   $(function(){
+     $.ajaxSetup({
+      headers:{
+      "X-CSRF-TOKEN":$('meta[name="csrf-token"]').attr('content')
+      }
+     }); 
+   });
+   name = $('#sagaeee').val();
+   email = $('#sagar').val();
+   $('#submit').click(function(e){
+   e.preventDefualt();
+   $.ajax({
+      url:'/insert',
+      data:name:name,email:email,
+      type:'POST',
+      success:function()
+      {
+       $('#message').text('text');
+      }, 
+      error:function()
+      {  
+      $("#message").text('text');
+      }
+   }); 
+   });
+</script>
+
+
+
--- resources/views/answer.blade.php
+++ resources/views/answer.blade.php
@@ -1,171 +1,81 @@
 <?php
-$c = 11;
-$a = '9';
-$b = 18; 
-if($a === 10)
+$n = 21;
+if($n >10)
+{
+  echo "how to create";
+}
+if($n > 20)
+{
+  echo "rtjjiorjtr";
+}
+else
+{ 
+ echo "insert";
+}
+if($n > 10)
+{
+  echo "ledder";     
+} 
+elseif($n > 10)
+{
+  echo "ledder";  
+} 
+else
+{
+  echo "ledder if";
+}
+if($n > 10)
   {
-    echo 1;
-  }
-if($b != 10)
-  {
-    class Name
-    {
-      public $value;
-      function __construct($value)
+    echo "nested";
+    if($n >20)
       {
-        $this->value = $value;
+       echo "two nested";
       }
-    }
-    abstract class sagar2
-    {
-     public Name $name;
-      function __construct(Name $name)
-      {
-        $this->name = $name;
-      }
-      public abstract function fg();
-    }
-    class sd extends sagar2
-    {
-      function fg()
-      {
-        echo $this->name->value;
-      }
-    }
-    $objs = new Name('sds');
-    $obj = new sd($objs);
-    $obj -> fg();
-    echo "check";
   }
-if($b += 10)
-  {
-    echo "less check";
-  }
-if($c <= 10)
-  {
-    echo "greth check";
-  }
-  $name = 8;
-$status = ($name < 10) ? "false" : "true";
-echo $status;
-interface fg{
-  public function name();
-}
-class dg implements fg{
-  public function name()
-  {
-    echo "q";
-  }
-}
-class ct implements fg{
-  public function name()
-  {
-    echo "q";
-  }
-}
-$c = new dg();
-$c->name();
-$b = new ct();
-$b->name();
-trait het 
+  $a = 000;
+switch($a)
 {
-  public function ambaliya()
-  {
-    echo "1","2","3";
-  }
+case 11:
+  echo "elevent";
+  break;
+  case 10:
+    echo "ten";
+    break;
+ default:
+ echo "no one there";
 }
-class mnasi 
-{
-  use het;
-}
-$net = new mnasi();
-$net-> ambaliya();
-class yyy
-{
-  private $amout;
-    function __construct($amoute)
-    {
-       $this->amoute = $amoute;
-    }
-    function get_value($value)
-    {
-      $this->amoute -= $value;
-      return $this->amoute;
-    }
-}
-$obj = new yyy(5000);
-echo  $obj->get_value(800);
-if($a < 10)
-{
-echo '<!DOCTYPE html>
-<html>
-<head>
-  <title>Enter Open Modal</title>
-    <style>
-        .modal {
-            display: none;
-            position: fixed;
-            top: 0; left: 0;
-            width: 100%; height: 100%;
-            background: rgba(0,0,0,0.s5);
-        }
-        .modal-content {
-            background: white;
-            padding: 20px;
-            width: 300px;
-            margin: 100px auto;
-        }
-    </style>
-</head>
-<body>
-    <h3>Press Enter to open modal</h3>
-    <input type="text" placeholder="Press Enter here">
-    <div class="modal" id="myModal">
-        <div class="modal-content">
-            <p>Modal Opened</p>
-            <button onclick="closeModal()">Close</button>
-        </div>
-    </div>
-    <script>
-        document.addEventListener("keydown", function(event) 
-        {
-            if (event.key === "Enter") 
-            {
-                document.getElementById("myModal").style.display = "block";
-            }
-        });
-        function closeModal() 
-        {
-            document.getElementById("myModal").style.display = "none";
-        }
-    </script>
-</body>
-</html>';
-}
-$name = 101010.10;
-$status = ($name < 10 ) ? "false":"true";
-echo $status;
-var_dump($name);
-trait jit
-{
-  public function hit()
-  {
-    echo "rettret";
-  }
-}
-class we 
-{
-     use jit;
-}
-$neet = new we();
-$neet->hit();
 ?>
+<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+<button id='submit'>submit</button>
+<input id='search'/>
+<p id="p">sdfghj</p>
+<div id="box">Hover me</div>
+<div id="tooltip" style="display:none; position:absolute;">Tooltip text</div>
 <script>
   $(function(){
-   $.ajaxSetup({
-    headers:{
-      'X-CSRF-TOKEN':$('meta(name="csrf-token")').attr('content')
-    }
+   $('#submit').submit(function ()
+   {
+    $('#p').show();
+   });  
+    $('#submit').dblclick(function (){
+      $('#p').hide();
    });
+   $('#search').on('input',function()
+  {
+    let value = $(this).val();
+    console.log(value);
   });
+  });
+  $('#box').mouseenter(function () {
+  $('#tooltip').show();
+});
+$('#box').mousemove(function (e) {
+    $('#tooltip').css({
+        top: e.pageY + 10,
+        left: e.pageX + 10
+    });
+});
+$('#box').mouseleave(function() {
+    $('#tooltip').hide();
+});
 </script>
